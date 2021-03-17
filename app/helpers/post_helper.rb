9430d0eb456b3e405b5c45a41728a272760da736
george-shammar <george.gbenle@outlook.com>
2021-03-17 16:06:52 +0000

    Implement like count

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -5,3 +5,4 @@ module PostHelper
     content_tag :p, "Post could not be saved. #{post.errors.full_messages.join('. ')}", class: 'errors'
   end
 end
+
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,3 +8,8 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 end
+
+def like_count(post)
+  @like = Like.where(post_id: post.id)
+  @like.count
+end
