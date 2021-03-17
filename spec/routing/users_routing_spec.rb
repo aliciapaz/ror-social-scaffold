dc48410c8d0f2cef87194a0583035647d1f12443
george-shammar <george.gbenle@outlook.com>
2021-03-17 21:22:04 +0000

    Create test for Users controller index route

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe UsersController, type: :routing do
         it 'routes to #show' do
             expect(get: 'users/1').to route_to('users#show', id: '1')
         end
+
+        it 'routes to #index' do
+            expect(get: "/users" ).to route_to('users#index')
+        end
     end
 end
